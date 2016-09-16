3d6fb3d35e4036c6f437c813c51a7fc4e1964307
Chad Duffey <chadduffey@hotmail.com>
2016-09-16 14:22:39 -0700

    error handling

--- GradeBook2/GradeBook.cs
+++ GradeBook2/GradeBook.cs
@@ -26,7 +26,7 @@ namespace GradeBook
         {
             for (int i = grades.Count; i > 0; i--)
             {
-                destination.WriteLine(grades[i -1]);
+                destination.WriteLine(grades[i - 1]);
             }
         }
 
@@ -38,19 +38,23 @@ namespace GradeBook
             }
             set
             {
-                if (!String.IsNullOrEmpty(value))
+
+                if (string.IsNullOrEmpty(value))
                 {
-                    if (_name != value)
-                    {
-                        NameChangedEventArgs args = new NameChangedEventArgs();
-                        args.ExistingName = _name;
-                        args.NewName = value;
-
-                        NameChanged(this, args);
-                    }
-
-                    _name = value;
+                    throw new ArgumentException("Name cannot be null or empty");
                 }
+
+                if (_name != value)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+
+                    NameChanged(this, args);
+                }
+
+                _name = value;
+
             }
         }
 
@@ -60,7 +64,7 @@ namespace GradeBook
 
             float sum = 0;
 
-            foreach(float grade in grades)
+            foreach (float grade in grades)
             {
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
                 stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
